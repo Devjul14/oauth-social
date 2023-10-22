1b243b8728b61fe583c39f12057f26bfe8204cdb
julia eka <eka47577@gmail.com>
2023-10-22 08:14:51 +0700

    fix callback

--- config/services.php
+++ config/services.php
@@ -32,6 +32,6 @@ return [
      'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/login/facebook/callback',
+        'redirect' => 'https://oauth.juliadev.website/login/facebook/callback',
      ],
 ];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    {{ __('Hello!') }}
                 </div>
             </div>
         </div>
